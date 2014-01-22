d7c11ab2633a3bf5fe748424ce8d1f514a690389
Abhijith Settipalli <settiab@gmail.com>
2014-01-22 01:26:09 -0500

    initial client app

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.stat = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Accept Queue";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Ready";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // stat
+            // 
+            this.stat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stat.Location = new System.Drawing.Point(0, 0);
+            this.stat.Name = "stat";
+            this.stat.Size = new System.Drawing.Size(164, 24);
+            this.stat.TabIndex = 2;
+            this.stat.Text = "Idle";
+            this.stat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.stat);
+            this.panel1.ForeColor = System.Drawing.SystemColors.Info;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 24);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(91, 43);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(85, 57);
+            this.panel2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connect Code";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(0, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CODE";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 163);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LoL AFK Accept";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label stat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Windows API
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Collections.Specialized;
+//Window Focus changed hooks
+//using System.Windows.Automation;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        IntPtr clientHandle = IntPtr.Zero;
+        double xRatio = 0.4246;
+        double yRatio = 0.5638;
+        Boolean isReady = false;
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out Point lpPoint);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        private const int HSHELL_WINDOWCREATED = 1;
+        public delegate bool WindowEnumCallback(int hwnd, int lparam);
+        private int uMsgNotify;
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnumWindows(WindowEnumCallback lpEnumFunc, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr h, StringBuilder s, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(int h);
+        [DllImport("user32.dll")]
+        private static extern int RegisterWindowMessage(string lpString);
+        [DllImport("user32.dll")]
+        private static extern int RegisterShellHookWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern void FlashWindow(IntPtr a, bool b);
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool CloseWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool BringWindowToTop(IntPtr hWnd);
+
+        //Window Focus Change
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        //Get Window Bounds
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        public static Size GetWindowSize(IntPtr hWnd)
+        {
+            RECT pRect;
+            Size cSize = new Size();
+            // get coordinates relative to window
+            GetWindowRect(hWnd, out pRect);
+
+            cSize.Width = pRect.Right - pRect.Left;
+            cSize.Height = pRect.Bottom - pRect.Top;
+
+            return cSize;
+        }
+
+
+        public Form1()
+        {
+            Console.WriteLine("Form initialized");
+            InitializeComponent();
+            WinEventDelegate dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.Write("Accept button clicked for");
+            Console.WriteLine(clientHandle.ToString());
+            if (clientHandle != IntPtr.Zero)
+            {
+                acceptQueue(clientHandle);
+                isReady = false;
+                button1.Enabled = false;
+                stat.Text = "Idle";
+                stat.ForeColor = SystemColors.Info;
+            }
+            else
+                MessageBox.Show("Queue not ready!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!isReady)
+            {
+                Console.WriteLine("Ready to detect queue pop");
+                isReady = true;
+                stat.Text = "Waiting for queue";
+                stat.ForeColor = System.Drawing.Color.Red;
+                button2.Text = "Cancel";
+            }
+            else
+            {
+                Console.WriteLine("Ready cancelled");
+                isReady = false;
+                button2.Text = "Ready";
+                stat.Text = "Idle";
+                stat.ForeColor = SystemColors.Info;
+            }
+        }
+
+        private void acceptQueue(IntPtr handle)
+        {
+            //Bring window to the top
+            BringWindowToTop(handle);
+            //Get Window bounds and calculate button coords
+            Point button = getButtonCoordinates(handle);
+            ClientToScreen(handle,ref button);
+            //Click button
+            ClickOnPoint(button);
+            //Reset state
+            Console.WriteLine("---QUEUE ACCEPTED---");
+        }
+
+        private Point getButtonCoordinates(IntPtr handle)
+        {
+            //Rectangle resolution = Screen.PrimaryScreen.Bounds;
+            //return new Point(BUTTON_X * resolution.Width / 1920, BUTTON_Y * resolution.Height / 1080);
+            Size bounds = GetWindowSize(handle);
+            int buttonX = (int)(xRatio * bounds.Width);
+            int buttonY = (int)(yRatio * bounds.Height);
+            Console.WriteLine("X: " + buttonX.ToString() + " Y: " + buttonY.ToString());
+            return new Point(buttonX, buttonY);
+        }
+
+        private void ClickOnPoint(Point clientPoint)
+        {
+            Point oldPoint;
+            GetCursorPos(out oldPoint);
+
+            /// set cursor on coords, and press mouse
+            SetCursorPos(clientPoint.X, clientPoint.Y);
+            mouse_event(0x00000002, 0, 0, 0, UIntPtr.Zero); /// left mouse button down
+            mouse_event(0x00000004, 0, 0, 0, UIntPtr.Zero); /// left mouse button up
+
+            /// return mouse 
+            SetCursorPos(oldPoint.X, oldPoint.Y);
+        }
+
+        private IntPtr detectQueuePop()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0 && Buff.ToString() == "PVP.net Client")
+            {
+                return handle;
+            }
+            return IntPtr.Zero;
+        }
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            IntPtr tempClientHandle = detectQueuePop();
+            if (tempClientHandle != IntPtr.Zero && isReady)
+            {
+                clientHandle = tempClientHandle;
+                Console.WriteLine("QUEUE DETECTED!!!");
+                Console.WriteLine(clientHandle.ToString());
+                stat.Text = "Queue Ready";
+                button1.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            return;
+        } 
+
+
+        
+    }
+}
